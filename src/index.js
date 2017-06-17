fe2f3188dfc3cf000cddb076967c37cb51c77bdb
Priit Pärl <priitparl@gmail.com>
2017-06-17 23:46:24 +0300

    App component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YouTubeSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyD88Xj0Rg6uw6f4uyRy_B1pdNN-wHciGBY';
+
 // Create a new component
 // Component should produce some html
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
-            <SearchBar />
-        </div>
-    )
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YouTubeSearch({key: API_KEY, term: 'surfboards'}, data => {
+            this.setState({ videos: data });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // Take components html and put into the DOM

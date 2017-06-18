c921259f4b7fd403edbb2e0efe75a4b6fce2f42a
Priit Pärl <priitparl@gmail.com>
2017-06-18 11:20:52 +0300

    search input, load items, css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YouTubeSearch from 'youtube-api-search';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyD88Xj0Rg6uw6f4uyRy_B1pdNN-wHciGBY';
 
 // Create a new component
@@ -12,18 +14,34 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YouTubeSearch({key: API_KEY, term: 'surfboards'}, videos => {
-            this.setState({ videos });
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YouTubeSearch({key: API_KEY, term: term}, videos => {
+            this.setState({
+                videos:videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
     render() {
+
+        const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
